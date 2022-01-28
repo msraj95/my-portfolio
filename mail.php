5d6585a939db61f04bc5bd9e099f0714e39c6080
Mohammad Seraj <83978726+msraj95@users.noreply.github.com>
2022-01-28 13:32:56 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,4 +1,41 @@
 <?php
+    $errors = '';
+    $myemail = 'Qmdseraj@gmail.com';
+    if(empty($_POST['email'])  ||
+       empty($_POST['name']) ||
+       empty($_POST['mobile']))||
+       empty($_POST['subject']))||
+       empty($_POST['message']))
+    {
+    $errors .= "\n Error: Required Field";
+    }
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $contact_phone=$_POST['mobile'];
+    $contact_service=$_POST['subject'];
+    $message = $_POST['message'];
+
+
+    if( empty($errors)){
+        $to = $myemail;
+        $email_subject = "Contact Form: Seraj Hassan";
+        $email_body='<h3>You have received a new message. Details are given below.</h3>
+        <table width="100%" border="1" cellspacing="1" cellpadding="1">
+        <tr><td style="padding:10px;">Email: </td><td style="padding:10px;">'.$email.'</td></tr>
+        <tr><td style="padding:10px;">Name: </td><td style="padding:10px;">'.$name.'</td></tr>
+        <tr><td style="padding:10px;">Message: </td><td style="padding:10px;">'.$message.'</td></tr>
+        </table>';
+
+        $headers = "From: " . $email. "\r\n";
+        $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+        mail($to, $email_subject, $email_body, $headers);
+    }
+?>
+/* 
+<?php
 $page_success = "sent.html";
 $page_failed = "failed.html";
 
@@ -40,4 +77,4 @@ else
 
    header("Refresh: 0; url=$page_failed");
 }
-?>
+?> */
