c9941b3b53a7838fd4880907d3dbdffdf8b320fb
SERAJ HASSAN <qmdseraj@gmai.com>
2022-02-05 23:33:46 +0530

    changed

--- mail.php
+++ mail.php
@@ -1,15 +1,15 @@
-<?php
+git <?php
 $page_success = "sent.html";
 $page_failed = "failed.html";
 
-$contact_name=$_POST['txtname'];
-$contact_email=$_POST['txtemail'];
-$contact_phone=$_POST['txtnumber'];
-$contact_service=$_POST['txtservice'];
-$contact_message=$_POST['txtcomment'];
+$contact_name=$_POST['name'];
+$contact_email=$_POST['mail'];
+$contact_phone=$_POST['mobile'];
+$contact_service=$_POST['subject'];
+$contact_message=$_POST['message'];
 $to_email = 'Qmdseraj@gmail.com';
-$to_ccemail = 'esoft.msraj5795@gmail.com';
-//$to_ccccemail = 'shinithet@gmail.com';
+$to_ccemail = 'seraj@alityart.com';
+
 //you can give email id to whom you need to send
 $html = 'New Message From Website';
 $subject =  $html;
@@ -25,7 +25,7 @@ $message = "You have received a new message from contact us page:\n\n".
     "Message: $contact_service.\n\n".
 
 $headers .= $email_sub;
-$headers .= 'From: Mordecailegolas' . "\r\nReply-to: $contact_email"; //DONT GIVE SPACE IN "ABC"  //you can replace your value but no space.. if u give space you can get email in spam only..
+$headers .= 'From: SerajPortfolio' . "\r\nReply-to: $contact_email"; //DONT GIVE SPACE IN "ABC"  //you can replace your value but no space.. if u give space you can get email in spam only..
 $response = mail($to_email,$subject,$message,$headers);
 mail($to_ccemail,$subject,$message,$headers);
 //mail($to_ccccemail,$subject,$message,$headers);
